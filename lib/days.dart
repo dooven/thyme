e9319cc6039154d9a5840d1ebd65188c31ec3778
Daven Casia <davencasia@gmail.com>
2020-06-21 12:11:12 -0600

    Remove the default flutter icon, and add the new icon

--- lib/days.dart
+++ lib/days.dart
@@ -1,5 +1,5 @@
 class Days {
-  static List<String> DAY_LETTER = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+  static const List<String> DAY_LETTER = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
 
   static String dayNumberToLetter(int dayNumber) {
     return DAY_LETTER[dayNumber];
